0d9d907f481ca8e49e53746a1cea8d5f6d957359
lsilvpin <lsilvpin@everis.com>
2020-07-29 17:59:05 -0300

    Implementacao de um EventHandler que monitora a mudanca nos campos de um objeto!

---
+++ EventHandlerChanllenge/Program.cs
@@ -0,0 +1,83 @@
+﻿using EventHandlerChanllenge.Model;
+using System;
+
+namespace EventHandlerChanllenge
+{
+  /// <summary>
+  /// Implementação de um EventHandler que monitora a mudança dos campos de um objeto
+  /// </summary>
+  class Program
+  {
+    // Coisa que será monitorada
+    private static Thing thing;
+
+    static void Main(string[] args)
+    {
+      // Instancia a coisa
+      thing = new Thing("Goku", 45, true);
+
+      // Apresenta a coisa
+      Console.WriteLine($"O estado inicial da coisa é: {thing.Name}, {thing.Age}, {thing.ActualState()}");
+
+      // Marca a coisa como 'Monitorada'
+      thing.guarded = true;
+
+      // Inicia o processo de comuniação com o usuário
+      while (true)
+      {
+        Console.Write("Comando: ");
+        CommandHandler(Console.ReadLine());
+      }
+    }
+
+    /// <summary>
+    /// Trata as entradas do usuário no console
+    /// </summary>
+    /// <param name="command"></param>
+    private static void CommandHandler(string command)
+    {
+      if (command == "Ver coisa")
+      {
+        Console.WriteLine("Identifiquei que você deseja saber o estado atual da coisa!");
+        Console.WriteLine("Muito bem então, vamos lá!");
+        Console.WriteLine($"A coisa se chama {thing.Name}.");
+        Console.WriteLine($"A idade da coisa é {thing.Age}.");
+        Console.WriteLine($"A coisa está {thing.ActualState()}.");
+      }
+      else if (command == "Mudar nome")
+      {
+        Console.WriteLine("Identifiquei que você deseja alterar o nome da coisa!");
+        Console.Write("Qual será o novo nome? ");
+        thing.Name = Console.ReadLine();
+      }
+      else if (command == "Mudar idade")
+      {
+        Console.WriteLine("Identifiquei que você deseja alterar a idade da coisa!");
+        Console.Write("Qual será a nova idade? ");
+        thing.Age = Convert.ToInt32(Console.ReadLine()); // Faltaria validar tipo da entrada
+      }
+      else if (command == "Matar")
+      {
+        Console.WriteLine("Identifiquei que você deseja matar a coisa!");
+        thing.Alive = false;
+      }
+      else if (command == "Ressuscitar")
+      {
+        Console.WriteLine("Identifiquei que você deseja ressuscitar a coisa!");
+        thing.Alive = true;
+      }
+      else if (command == "Exit")
+      {
+        Console.WriteLine("Identifiquei que você deseja que eu finalize o programa!");
+        Console.WriteLine("Tudo bem! Volte sempre!");
+        Console.WriteLine("Aperte qualquer tecla para finalizar ...");
+        Console.ReadLine();
+        Environment.Exit(0);
+      }
+      else
+      {
+        Console.WriteLine("Desculpe, não entendi o que você deseja! Tente novamente por gentileza.");
+      }
+    }
+  }
+}
---
+++ EventHandlerChanllenge/Thing.cs
@@ -0,0 +1,154 @@
+﻿using System;
+
+namespace EventHandlerChanllenge.Model
+{
+  class Thing
+  {
+    // Atributos
+    private string _name;
+    private int _age;
+    private bool _alive;
+
+    /// <summary>
+    /// Constrói a coisa com suas características iniciais
+    /// </summary>
+    /// <param name="name">Name</param>
+    /// <param name="age">Idade</param>
+    /// <param name="alive">Estado de vida</param>
+    public Thing(string name, int age, bool alive)
+    {
+      _name = name;
+      _age = age;
+      _alive = alive;
+    }
+
+    // Monitorada?
+    public bool guarded { get; set; }
+
+    // Propriedades
+    public string Name
+    {
+      get { return _name; }
+      set
+      {
+        if (guarded)
+        {
+          string oldName = _name;
+          _name = value;
+
+          if (oldName != _name)
+          {
+            OnNameChange(oldName, _name);
+          }
+          else
+          {
+            Console.WriteLine("Ops! O nome que você inseriu já é o nome da coisa!");
+          }
+        }
+        else
+        {
+          _name = value;
+        }
+      }
+    }
+    public int Age
+    {
+      get { return _age; }
+      set
+      {
+        if (guarded)
+        {
+          int oldAge = _age;
+          _age = value;
+
+          if (oldAge != _age)
+          {
+            OnAgeChange(oldAge, _age);
+          }
+          else
+          {
+            Console.WriteLine("Ops! A idade que você inseriu já é a idade da coisa!");
+          }
+        }
+        else
+        {
+          _age = value;
+        }
+      }
+    }
+    public bool Alive
+    {
+      get { return _alive; }
+      set
+      {
+        if (guarded)
+        {
+          bool oldAlive = _alive;
+          _alive = value;
+
+          if (oldAlive != _alive)
+          {
+            OnAliveChange(_alive);
+          }
+          else
+          {
+            if (value)
+            {
+              Console.WriteLine("Amigo, não se ressuscita o que já está vivo!");
+            }
+            else
+            {
+              Console.WriteLine("Amigo, não se mata o que já está morto!");
+            }
+          }
+        }
+        else
+        {
+          _alive = value;
+        }
+      }
+    }
+
+    /// <summary>
+    /// Traduz para o português o estado de vida da coisa
+    /// </summary>
+    /// <returns></returns>
+    public string ActualState()
+    {
+      if (Alive)
+      {
+        return "Viva";
+      }
+      else
+      {
+        return "Morta";
+      }
+    }
+
+    // Quando a instância está sendo monitorada
+    #region EventHandler
+    /// <summary>
+    /// Quando o nome é alterado
+    /// </summary>
+    private void OnNameChange(string oldName, string newName)
+    {
+      Console.WriteLine($"O nome foi alterado de {oldName} para {newName}!");
+    }
+    /// <summary>
+    /// Quando a idade é alterada
+    /// </summary>
+    private void OnAgeChange(int oldAge, int newAge)
+    {
+      Console.WriteLine($"A idade foi alterada de {oldAge} para {newAge}!");
+    }
+    /// <summary>
+    /// Quando a vida é tirada
+    /// </summary>
+    private void OnAliveChange(bool actualState)
+    {
+      string result = (actualState) ? "ressuscitada" : "assassinada";
+      Console.WriteLine($"A coisa foi {result}!");
+    }
+    #endregion
+  }
+}
